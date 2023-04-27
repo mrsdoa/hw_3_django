6a5a93d5f4b291a937ec388bab9f83daa6a7b672
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-04-27 07:37:58 +0300

    Add files via upload

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,7 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
-
+from django.template.defaultfilters import slugify
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -10,8 +10,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
-
-        for phone in phones:
+            phone_list = csv.reader(file, delimiter=';')
+            # phones = list(csv.DictReader(file, delimiter=';'))
+            next(phone_list)  # не читаем 1-ю строку csv
+            for phone in phone_list:
             # TODO: Добавьте сохранение модели
-            pass
+                one_line = Phone.objects.create(
+                    id = int(phone[0]),
+                    name = phone[1],
+                    image = phone[2],
+                    price = int(phone[3]),
+                    release_date = phone[4],
+                    lte_exist = phone[5],
+                    slug = slugify(phone[1]),
+                )
