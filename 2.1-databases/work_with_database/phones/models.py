cf3f67064f678b7de3ea08ffa6bc695c0b839b58
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-04-27 07:37:00 +0300

    Add files via upload

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
-
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=250)
+
+    # мб текст ниже и не нужен
+    def __str__(self):
+        return f'{self.name, self.price, self.image, self.release_date, self.lte_exists, self.slug}'
